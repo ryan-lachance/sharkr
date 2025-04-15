c13bb5f2f68cd279913ead81fda11bba1f61d56c
ryan-lachance <r-lachance@hotmail.com>
2025-04-15 12:40:25 -0400

    Revert "Attempted fix of authetnication"
    This reverts commit bc3b94708bf6326f0d88fea7c0c178de119605ce.

--- server/server.js
+++ server/server.js
@@ -32,8 +32,6 @@ app.use(
     resave: false,
     cookie: {
       maxAge: 60000 * 60 * 24,
-      sameSite: "none",
-      secure: true,
     },
     saveUninitialized: false,
     store: MongoStore.create({
