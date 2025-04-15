efe62e5804015f1627f461235a36bf190ba6cc38
ryan-lachance <r-lachance@hotmail.com>
2025-04-15 13:03:11 -0400

    Undid changes

--- server/server.js
+++ server/server.js
@@ -32,8 +32,6 @@ app.use(
     resave: false,
     cookie: {
       maxAge: 60000 * 60 * 24,
-      secure: true,
-      sameSite: "None",
     },
     saveUninitialized: false,
     store: MongoStore.create({
