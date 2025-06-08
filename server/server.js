59e74db9d2ff4c4ad0835ab275158d35b9e3fe5a
ryan-lachance <r-lachance@hotmail.com>
2025-06-08 01:44:07 -0400

    Scroll fix

--- server/server.js
+++ server/server.js
@@ -1,18 +1,9 @@
-//Dependencies
-require("dotenv").config();
-const env = process.env;
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const loanRoutes = require("./routes/loans");
 const authRoutes = require("./routes/auth");
 const guildRoutes = require("./routes/guilds");
-const bot = require("./bot");
-const cron = require("node-cron");
 const session = require("express-session");
-const passport = require("passport");
-const DiscordStrategy = require("./strategies/discordstrategy");
-const MongoStore = require("connect-mongo");
 
 //express app
 const app = express();
@@ -22,31 +13,26 @@ app.set("trust proxy", 1);
 app.use(express.json());
 app.use(
   cors({
-    origin: env.CLIENT_URL,
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 app.use(
   session({
-    secret: env.CORS_SECRET,
+    secret: "secret",
     resave: false,
     cookie: {
       maxAge: 60000 * 60 * 24,
-      sameSite: "none", //Comment out for local dev
-      secure: true, //Comment out for local dev
+      //sameSite: "none", //Comment out for local dev
+      //secure: true, //Comment out for local dev
     },
     saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: env.MONGO_URI,
-      collectionName: "sessions",
-    }),
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use((req, res, next) => {
+  req.user = req.session.user;
   next();
 });
 
@@ -55,23 +41,6 @@ app.use("/api/loans", loanRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/guilds", guildRoutes);
 
-// connect to db
-mongoose
-  .connect(env.MONGO_URI)
-  .then(() => {
-    app.listen(env.PORT, () => {
-      console.log("Connected to DB and listening on Port 2000!");
-    });
-  })
-  .catch((error) => {});
-
-cron.schedule(
-  "0 0 * * *",
-  () => {
-    bot.maintain();
-  },
-  {
-    scheduled: true,
-    timezone: "America/New_York",
-  }
-);
+app.listen(2000, () => {
+  console.log("Listening on Port 2000!");
+});
